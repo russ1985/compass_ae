63c25ab8683c9e6f54fd59d6cc55c19261ddaf42
Russell Holmes <russellfholmes@gmail.com>
2014-02-08 17:23:40 -0500

    fixed bug in saas config

--- config/initializers/compass_ae_saas.rb
+++ config/initializers/compass_ae_saas.rb
@@ -1,6 +1,6 @@
 CompassAeSaas::Config.configure do |config|
   config.master_url = 'https://master.mycompassagile.com'
-  config.compass_ae_security_token = 'bd7b19c0-a92d-11e2-9e96-0800200c9a66',
+  config.compass_ae_security_token = 'bd7b19c0-a92d-11e2-9e96-0800200c9a66'
   config.validate_instances = true
 end
 
